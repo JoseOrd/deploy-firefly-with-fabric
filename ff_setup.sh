500911e46f071db3ea62208922154bb106646c62
JoseOrd <jose.exe6910@gmail.com>
2024-06-18 15:20:20 -0600

    chore: Update FireFly configuration files for org2 deployment

---
+++ ff_setup.sh
@@ -0,0 +1,3 @@
+cd ..
+curl -sSLO https://raw.githubusercontent.com/JoseOrd/deploy-firefly-with-fabric/master/org1_ccp.yml
+curl -sSLO https://raw.githubusercontent.com/JoseOrd/deploy-firefly-with-fabric/master/org2_ccp.yml
