93d1316cc5ea43d124170157549e7e7ffd664d29
JoseOrd <jose.exe6910@gmail.com>
2024-06-26 17:33:03 -0600

    fix:change network directory

--- ff_setup.sh
+++ ff_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Lookup for the Fabric binaries
-FABRIC_TEST_NETWORK="$PWD/network-deploy/fabric-samples/test-network"
+FABRIC_TEST_NETWORK="$PWD/fabric-samples/test-network"
 ORGANIZATIONS="$FABRIC_TEST_NETWORK/organizations"
 ORG1_USER_KEYSTORE_DIR="$ORGANIZATIONS/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/"
 ORG2_USER_KEYSTORE_DIR="$ORGANIZATIONS/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp/keystore/"
-FABRIC_BINS=$PWD/network-deploy/fabric-samples/bin
+FABRIC_BINS=$PWD/fabric-samples/bin
 
 # Config
 RED='\033[0;31m'
@@ -18,9 +18,7 @@ echo -e "${GREEN}Iniciando la configuración de FireFly con Fabric...${NC}"
 echo -e "${GREEN}Para cancelar, presiona CTRL+C.${NC}"
 sleep 5
 
-mkdir network-deploy
-cd network-deploy/
-
+# Install prerequisites
 curl -sSLO https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh && chmod +x install-fabric.sh
 ./install-fabric.sh docker samples binary
 
